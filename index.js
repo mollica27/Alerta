43ce5a2bb533a5cf6317a1fa6da4559c0acf2932
Natan Mollica <natan.mollica@gmail.com>
2022-08-13 19:24:33 -0300

    criando conteudo index.ejs

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const path = require("path");
-const routes = require("./routes/routes")
+const routes = require("./routes/routes");
+const connectToDb = require("./database/db");
 
+connectToDb();
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname,"public")));
-app.use(routes)
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+app.use(routes);
+
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
---
+++ models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { isModuleNamespaceObject } = require('util/types');
+
+const taskSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        require: true
+    },
+    check: {
+        type: Boolean,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
